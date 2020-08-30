72560616c73e6014205d81cf733bf236a102eca0
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-08-30 19:59:21 +0900

    404調整

--- node/sam.js
+++ node/sam.js
@@ -1,31 +1,34 @@
 let http = require("http");
 let fs = require("fs");
+const { deflateRaw } = require("zlib");
+
+let indexPage = fs.readFileSync("./index.html","utf-8");
+let nextPage = fs.readFileSync("./next.html","utf-8");
 
 let server = http.createServer(function(req,res){
-    let target = "";
-    switch (req.url){
+    let target ="";
+    switch(req.url){
         case "/":
-        case "/index":
-            target ="./index.html";
-            break;
-        case "/next":
-            target ="./next.html";
-            break;
-       default:
-           res.writeHead(404,{
-               "Content-Type":"text/plain"
-           });
-           res.end("bad request");
-           return;
-    }
-    fs.readFile(target,"utf-8",function(err,data){
-        res.writeHead(200,{
-            "Content-Type":"text/html"
-        });
-        res.write(data);
-        res.end();
-    });
-});
+            case "/index":
+        target =indexPage;
+        break;
+    case "/next":
+        target = nextPage;
+        break;
+    default:
+        res.writeHead(404,{
+            "Content-Type":"text/plain"
+        })
+        res.end("not found");
+        return;
+    };
 
+    res.writeHead(200,{
+        "Content-Type":"text/html"
+    });
+    res.write(target);
+    res.end();
+
+});
 server.listen(1234,"127.0.0.1");
-console.log("server start");
+console.log("server start")
