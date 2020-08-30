18082d66bafd427202d914570476fe75fa8b4b6b
xxarupakaxx <arupaka3tdigks@gmail.com>
2020-08-30 22:34:04 +0900

    serverで管理

--- expre/app.js
+++ expre/app.js
@@ -1,16 +1,28 @@
-let http = require("http");
-var ejs = require("ejs");
+//let http = require("http");
+let ejs = require("ejs");
 let express = require("express");
-var app = express();
+let app = express();
+let bodyParser = require("body-parser");
 
 app.engine("ejs",ejs.renderFile);
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.get("/" ,function(req,res){
-    res.render("temp.ejs",{
-        contents:"<p>hoge</p>"
-    });
+    console.log("---GET---");
+    console.log("nameは" + req.query.name);
+    console.log("ageは" + req.query.age);
+    res.render("temp.ejs",{});
 });
 
+app.post("/",function(req,res){
+    console.log("--post---");
+    console.log("nameは" +req.body.name);
+    console.log("ageは" +req.body.age);
+    res.render("temp.ejs",{});
+    
+});
 let server = app.listen(1234,"127.0.0.1",function(){
     console.log("server");
 })
